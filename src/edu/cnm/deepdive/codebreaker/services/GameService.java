079917c7865a4798a518a7a8f9ecf92f0b2c93c1
Jason Rafferty <Jason0x55@gmail.com>
2018-06-10 17:49:49 -0600

    Added simple Client class to be able to make guesses and play a game.

--- src/edu/cnm/deepdive/codebreaker/services/GameService.java
+++ src/edu/cnm/deepdive/codebreaker/services/GameService.java
@@ -1,10 +1,29 @@
 package edu.cnm.deepdive.codebreaker.services;
 
 import edu.cnm.deepdive.codebreaker.models.Game;
+import edu.cnm.deepdive.codebreaker.models.Guess;
 import java.util.List;
+import java.util.UUID;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface GameService {
 
+  @GET("games")
   Call<List<Game>> list();
+
+  @GET("games/{uuid}")
+  Call<Game> findById(@Path("uuid") UUID uuid);
+
+  @POST("games")
+  Call<Game> createGame(@Body Game game);
+
+  @POST("games/{id}/guesses")
+  @Headers("accept:application/json")
+  Call<Guess> createGuess(@Path("id") UUID id, @Body Guess guess);
+
 }
